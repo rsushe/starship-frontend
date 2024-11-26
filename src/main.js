94e22597d0af6cc2f7c159ca07cf9e1cb0dde3ca
rsushe <sushenkoroman2003@gmail.com>
2024-11-26 23:48:10 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+
+const app = createApp(App);
+app.config.globalProperties.$axios = axios;
+app.mount('#app')
